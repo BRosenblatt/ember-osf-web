e374c56ec4e7cd393fff47148f13eb91b4fab9e6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-08 15:44:51 +0000

    Deploy CenterForOpenScience/ember-osf-web to github.com/CenterForOpenScience/ember-osf-web.git:gh-pages

---
+++ assets/node-asset-manifest.js
@@ -0,0 +1,6 @@
+/* eslint-disable */
+define('ember-osf-web/config/node-asset-manifest', function() {
+  return {
+    default: {"bundles":{"analytics-page":{"assets":[{"uri":"/ember-osf-web/engines-dist/analytics-page/assets/engine-47076e87b3d0b7d5a533a29d4c634fce.js","type":"js"},{"uri":"/ember-osf-web/engines-dist/analytics-page/assets/engine-de0c6909103d87479d0be4923c4df55e.css","type":"css"},{"uri":"/ember-osf-web/engines-dist/analytics-page/assets/engine-vendor-d820c81d18ac0a6506cc522bad3c7481.js","type":"js"},{"uri":"/ember-osf-web/engines-dist/analytics-page/assets/engine-vendor-e8543903832c2b83fa8ba84acbde8699.css","type":"css"}]},"handbook":{"assets":[{"uri":"/ember-osf-web/engines-dist/handbook/assets/engine-16b72cd9869e9c1061e7b9b82f0449d1.js","type":"js"},{"uri":"/ember-osf-web/engines-dist/handbook/assets/engine-4ce9e06cce48cf4c610ab7db5684f50c.css","type":"css"},{"uri":"/ember-osf-web/engines-dist/handbook/assets/engine-vendor-6ecbbeef34b25e2251bacabcd3f2a689.js","type":"js"},{"uri":"/ember-osf-web/engines-dist/handbook/assets/engine-vendor-e30934b92101e159b7415adc68317039.css","type":"css"}]}}}
+  };
+});
